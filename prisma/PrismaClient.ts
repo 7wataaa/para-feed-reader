b6119bd138adc4610c01790bce67ef9c05aee9d4
nashi <nn.contact.n@gmail.com>
2022-01-31 18:14:50 +0900

    Fix to cache Prisma Client

--- prisma/PrismaClient.ts
+++ prisma/PrismaClient.ts
@@ -1,5 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+/* 
+Next.jsの開発環境にて、
+"Error querying the database: db error: FATAL: 現在クライアント数が多すぎます"
+というエラーが起こるため、PrismaClientをグローバルにキャッシュする
+
+参考: https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+ */
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 export { prisma };
