b5aa04caeb919ac3b1121dbd5d1e61b49ada6eeb
nashi <nn.contact.n@gmail.com>
2022-01-31 18:37:51 +0900

    Add FeedIdOrderContext and FeedIdOrderProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import { FeedIdOrderProvider } from '../state/FeedIdOrderProvider';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <FeedIdOrderProvider>
+        <Component {...pageProps} />
+      </FeedIdOrderProvider>
     </SessionProvider>
   );
 }
